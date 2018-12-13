56003bf4026f12b73f549990b81bbb61b5932332
Denis Davydov <den.saint.k@gmail.com>
2018-12-14 01:14:03 +0300

    add adaptive for index page

--- build/assets/scripts/main.js
+++ build/assets/scripts/main.js
@@ -4,13 +4,45 @@ $(document).ready(function () {
     slidesToShow: 4,
     slidesToScroll: 1,
     prevArrow: $('.btn__btn--arrow-left'),
-    nextArrow: $('.btn__btn--arrow-right')
+    nextArrow: $('.btn__btn--arrow-right'),
+    responsive: [
+      {
+        breakpoint: 770,
+        settings: {
+          slidesToScroll: 1,
+          slidesToShow: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToScroll: 1,
+          slidesToShow: 1
+        }
+      }
+    ]
   });
 
   $('.reviews__slider').slick({
     slidesToShow: 4,
     slidesToScroll: 1,
     prevArrow: $('.reviews__btn-l'),
-    nextArrow: $('.reviews__btn-r')
+    nextArrow: $('.reviews__btn-r'),
+    responsive: [
+      {
+        breakpoint: 770,
+        settings: {
+          slidesToScroll: 1,
+          slidesToShow: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToScroll: 1,
+          slidesToShow: 1
+        }
+      }
+    ]
   });
 });
