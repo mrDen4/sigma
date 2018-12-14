5f3112ad0806e2a8cb17a1cb90ea550b4517a132
Denis Davydov <den.saint.k@gmail.com>
2018-12-14 16:31:23 +0300

    add sale page with adaptive and start build new page

--- build/assets/scripts/main.js
+++ build/assets/scripts/main.js
@@ -1,5 +1,18 @@
 $(document).ready(function () {
 
+  $('input[name=square]').nativeMultiple({
+    stylesheet: "slider",
+    onCreate: function() {
+        console.log(this);
+    },
+    onChange: function(first_value, second_value) {
+        console.log('onchange', [first_value, second_value]);
+    },
+    onSlide: function(first_value, second_value) {
+        console.log('onslide', [first_value, second_value]);
+    }
+  });
+
   $('.hot__slider').slick({
     slidesToShow: 4,
     slidesToScroll: 1,
