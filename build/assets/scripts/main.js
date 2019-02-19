21b66743b6a5f973ba7899a772ef215eb8d004bd
Denis Davydov <den.saint.k@gmail.com>
2019-02-19 11:25:35 +0300

    add tabs with bugs'

--- build/assets/scripts/main.js
+++ build/assets/scripts/main.js
@@ -1,3 +1,31 @@
+(function($){				
+  jQuery.fn.lightTabs = function(options){
+      
+      var createTabs = function(){
+          tabs = this;
+          i = 0;
+          
+          showPage = function(i){
+              $(tabs).children("div").children("div").hide();
+              $(tabs).children("div").children("div").eq(i).show();
+              $(tabs).children("ul").children("li").removeClass("active");
+              $(tabs).children("ul").children("li").eq(i).addClass("active");
+          }
+          
+          showPage(0);				
+          
+          $(tabs).children("ul").children("li").each(function(index, element){
+              $(element).attr("data-page", i);
+              i++;                        
+          });
+          
+          $(tabs).children("ul").children("li").click(function(){
+              showPage(parseInt($(this).attr("data-page")));
+          });				
+      };		
+      return this.each(createTabs);
+  };	
+})(jQuery);
 $(document).ready(function () {
 
   $('.contacts__btn, .bg, .popup__callback .callback__btn').on('click', function () {
@@ -17,6 +45,21 @@ $(document).ready(function () {
     $('.item__submenu--info').removeClass('item__submenu--active');
     $('.item__submenu--property').removeClass('item__submenu--active');
   });
+
+  $(".hot__container").lightTabs();
+
+  // $('.hot').on('click', ".btn__btn", function() {
+  //   var tabs = $('.hot__btns .btn__btn'),
+  //       cont = $('.hot__tabs .hot__tab');
+    
+  //   tabs.removeClass('hot__tab--active');
+  //   cont.removeClass('btn__btn--active');
+
+  //   $(this).addClass('active');
+  //   cont.eq($(this).index()).addClass("active");
+
+  //   return false;
+  // });
 
   $('.hot__slider').slick({
     slidesToShow: 4,
